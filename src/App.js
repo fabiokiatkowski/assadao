b25f9ff8c159cbc1b38c9fb172cb1b1f4c027c70
Fabio Kiatkowski <xolla@xolla.com.br>
2020-09-25 22:52:14 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Form, Modal, Table } from 'react-bootstrap';
+import { items } from './items';
+
+function FormPedido ({ visible=false, onClose, onSave, ...props }) {
+  const [pedido, setPedido] = useState({});
+  if (!visible) return null;
+  return (
+    <Modal
+      show={visible}
+      onHide={() => onClose(false)}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Nome do cliente</Form.Label>
+            <Form.Control
+              name="cliente"
+              placeholder="Digite o nome do cliente"
+              value={pedido.cliente}
+              onChange={(e) => setPedido({ ...pedido, cliente: e.target.value})}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            {
+              items.map(item => (
+                <>
+                  <Form.Label>{item.label}</Form.Label>
+                  <Form.Control
+                    name={item.value}
+                    placeholder="Quantidade"
+                    value={pedido[item.value]}
+                    onChange={(e) => setPedido({ ...pedido, [item.value]: e.target.value})}
+                  />
+                </>
+              ))
+            }
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Entrega" />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => onClose(false)}>Close</Button>
+        <Button variant="primary" onClick={() => onSave(pedido)}>Save changes</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
 
 function App() {
+  const [formPedidoVisible, setFormPedidoVisible] = useState(false);
+  const [pedidos, setPedidos] = useState([]);
+  const onSave = (pedido) => {
+    pedidos.push(pedido)
+    setPedidos(pedidos);
+    setFormPedidoVisible(false);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Assadão da Tere</h1>
+        <Button onClick={() => setFormPedidoVisible(true)}>Adicionar pedido</Button>
+        <FormPedido
+          visible={formPedidoVisible}
+          onClose={setFormPedidoVisible}
+          onSave={onSave}
+        />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Cliente</th>
+              {
+                items.map(item => <th>{item.label}</th>)
+              }
+            </tr>
+          </thead>
+          <tbody>
+            {
+              pedidos.map((pedido, index) => (
+                <tr key={pedido.cliente}>
+                  <td>{index}</td>
+                  <td>{pedido.cliente}</td>
+                  {
+                    items.map(item => <td>{pedido[item.value]}</td>)
+                  }
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
       </header>
     </div>
   );
---
+++ src/items.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    label: 'Frango de Carne',
+    value: 'FrangoCarne',
+  },
+  {
+    label: 'Frango de Aipim com Bacon',
+    value: 'FrangoAipim',
+  },
+  {
+    label: 'Frango sem recheio',
+    value: 'FrangoSemRecheio',
+  },
+  {
+    label: 'Costelinha',
+    value: 'Costelinha',
+  },
+  {
+    label: 'Costela Bovina',
+    value: 'CostelaBovina',
+  },
+  {
+    label: 'Pernil',
+    value: 'Pernil',
+  },
+  {
+    label: 'Linguicinha',
+    value: 'Linguicinha',
+  },
+];
+
+export { items };
